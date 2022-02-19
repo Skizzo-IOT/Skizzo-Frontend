f8ef8186821f8f8bce098d4dc6ac4a05fc1a2ea5
Nastassia Mana <nastassia.mana@gmail.com>
2022-02-19 12:27:33 +0100

    creation of login and welcome UI

--- skizzo/lib/main.dart
+++ skizzo/lib/main.dart
@@ -1,22 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:skizzo/screens/auth/auth_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const AuthScreen(),
-    );
+   return Scaffold(
+     body: SafeArea(
+       child: Container(
+         //double.infinity make it big as the parent allows
+         width: double.infinity,
+         height: MediaQuery.of(context).size.height,
+         padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           crossAxisAlignment: CrossAxisAlignment.center,
+           children: <Widget>[
+             Column(
+               children: <Widget>[
+                 Text("Skizzo", 
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                SizedBox(
+                  height: 20,
+                ),
+               ],
+             ),
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/homepage.jpg")
+                )
+              ),
+            ),
+            
+            Column(
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                  }, 
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Colors.black
+                  ),
+                  borderRadius: BorderRadius.circular(50)
+                ),
+                )
+              ],
+            ) 
+           ],
+         ),
+        )
+      )
+   );
   }
 }
