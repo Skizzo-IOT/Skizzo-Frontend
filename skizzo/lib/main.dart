e313e68ccc3e7deef14916640611297202495b41
Michaël BUGNONE <bugnone.michael@gmail.com>
2022-02-20 17:48:30 +0100

    clean main

--- skizzo/lib/main.dart
+++ skizzo/lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:skizzo/screens/auth/home.dart';
 import 'package:skizzo/video.dart';
 
-import 'login.dart';
-
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomePage(),
+    home: VideoPage(),
   ));
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: VideoPage(),
-    );
-  }
 }
