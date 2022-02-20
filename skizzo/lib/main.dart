0245a1ef846555732aaed5a206207d1a4bbe5f80
Michaël BUGNONE <bugnone.michael@gmail.com>
2022-02-20 15:45:23 +0100

    fix

--- skizzo/lib/main.dart
+++ skizzo/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login.dart';
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -10,60 +12,49 @@ void main() {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     body: SafeArea(
-       child: Container(
-         //double.infinity make it big as the parent allows
-         width: double.infinity,
-         height: MediaQuery.of(context).size.height,
-         padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: <Widget>[
-             Column(
-               children: <Widget>[
-                 Text("Skizzo", 
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ),
-                SizedBox(
-                  height: 20,
+    return Scaffold(
+        body: SafeArea(
+            child: Container(
+      //double.infinity make it big as the parent allows
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text(
+                "Skizzo",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
                 ),
-               ],
-             ),
-            Container(
-              height: MediaQuery.of(context).size.height / 3,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/homepage.jpg")
-                )
               ),
-            ),
-            
-            Column(
-              children: <Widget>[
-                MaterialButton(
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-                  }, 
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+          Image.asset("assets/homepage.jpg"),
+          Column(
+            children: <Widget>[
+              MaterialButton(
+                minWidth: double.infinity,
+                height: 60,
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginPage()));
+                },
                 shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    color: Colors.black
-                  ),
-                  borderRadius: BorderRadius.circular(50)
-                ),
-                )
-              ],
-            ) 
-           ],
-         ),
-        )
-      )
-   );
+                    side: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(50)),
+              )
+            ],
+          )
+        ],
+      ),
+    )));
   }
 }
